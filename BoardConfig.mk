ecb7abb901daed5daef4e90105d33cbf8e45b518
ezio84 <brabus84@gmail.com>
2017-01-12 00:01:28 +0100

    Lockscreen charging info: real time values [3/4]

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,6 +199,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/htc/pme/releasetools
 # RIL
 TARGET_RIL_VARIANT := caf
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
