rc420head/android_device_lge_h811

add my fav sounds
Change-Id: If1eee89c51c8a65efbb3724dc8162e775d306543

device.mk
@@ -21,7 +21,9 @@ $(call inherit-product-if-exists, vendor/lge/h811/h811-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/sounds/creek.mp3:system/media/audio/alarms/creek.mp3 \
+    $(LOCAL_PATH)/sounds/Iphone_Tritone.m4a:system/media/audio/notifications/Iphone_Tritone.m4a
 
 # Gps
 PRODUCT_COPY_FILES += \

DevHTC10/device_htc_pme

device.mk
@@ -107,7 +107,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
+    $(LOCAL_PATH)/sounds/creek.mp3:system/media/audio/alarms/creek.mp3 \
+    $(LOCAL_PATH)/sounds/Iphone_Tritone.m4a:system/media/audio/notifications/Iphone_Tritone.m4a
 
 # Email
 PRODUCT_PACKAGES += \