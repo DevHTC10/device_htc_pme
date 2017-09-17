8fc85c4b241cdd2af0ebcfd0fe2083a2650be7a2
Ethan Chen <intervigil@gmail.com>
2017-09-17 00:36:25 -0700

    pme: Add custom camera provider to handle user parameter
    * The hiae camera HAL passes the camera ID in the user field,
      but in Android 8.0, the user field is intended for a pointer
      to the CameraDevice object.
    
    Change-Id: Icc24ac8bd52b2c6d9756ed087de67c57c4b8f38e

--- device.mk
+++ device.mk
@@ -119,7 +119,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-pme \
     libshim_camera
 
 # Snap
